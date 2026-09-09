93e29a81062a3e9e60031424743d73ef53dc3e84
BOTREL Kilian <botrel.kilian1999@gmail.com>
2026-09-09 05:35:42 +0200

    chore(shared): remove dead code

--- src/shared/methods.ts
+++
@@ -1,9 +0,0 @@
-export const millisecondsToSeconds = (time: number) => {
-  return time / 1000;
-};
-
-export const computeThroughputPerSecond = (options: { duration: number; gain: number }) => {
-  const { duration, gain } = options;
-
-  return gain / duration;
-};
